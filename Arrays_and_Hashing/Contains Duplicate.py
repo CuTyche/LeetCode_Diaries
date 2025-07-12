e51b9b55d65b0f4627642bc374e91e996fb4cef8
Shravani Jaiswal <tychephp@gmail.com>
2025-07-12 10:00:13 +0530

    Update Contains Duplicate.py

--- Arrays_and_Hashing/Contains Duplicate.py
+++ Arrays_and_Hashing/Contains Duplicate.py
@@ -1,1 +1,13 @@
+#Problem: https://leetcode.com/problems/contains-duplicate/description/
+#Difficulty: Easy
+#Tags: Array, Hashing, Sorting
 
+class Solution(object):
+    def containsDuplicate(self, nums):
+        numset = set()
+        for num in nums:
+            if num in numset :
+                return True
+            else:
+                numset.add(num)
+        return False
