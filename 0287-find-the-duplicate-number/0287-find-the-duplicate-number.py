74146cf5113a2deb52fb4f97bc5965ba0696f09d
Shravani Jaiswal <tychephp@gmail.com>
2025-07-18 00:29:21 +0530

    Time: 51 ms (27.99%), Space: 20.8 MB (90.44%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return num
-            seen.add(num)
-        return -1
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
