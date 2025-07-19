22bdf33b3023e8804378659237e2ff75780b3168
Shravani Jaiswal <tychephp@gmail.com>
2025-07-19 15:51:33 +0530

    Time: 5 ms (98.06%), Space: 18.1 MB (57.91%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,21 +6,9 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:
-            return True
-        if not root:
-            return False
+        def serial(node):
+            if not node:
+                return '#'
+            return ','+ str(node.val) + serial(node.left)+ serial(node.right)
 
-        if self.sameTree(root, subRoot):
-            return True
-        return (self.isSubtree(root.left, subRoot) or 
-               self.isSubtree(root.right, subRoot))
-
-    def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and not subRoot:
-            return True
-        if root and subRoot and root.val == subRoot.val:
-            return (self.sameTree(root.left, subRoot.left) and 
-                   self.sameTree(root.right, subRoot.right))
-        return False
-        
+        return serial(subRoot) in serial(root)
