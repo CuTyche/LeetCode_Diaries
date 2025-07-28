8f7ddfdf259a88fa207f4d8a708c7633cd70a069
Shravani <tychephp@gmail.com>
2025-07-28 12:46:04 +0530

    Time: 1122 ms (5.02%), Space: 24 MB (94.63%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,5 +1,15 @@
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
-
+    def sortArray(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        gap = n // 2
+        while gap > 0:
+            for i in range(gap, n):
+                temp = arr[i]
+                j = i
+                while j >= gap and arr[j-gap] > temp:
+                    arr[j] = arr[j-gap]
+                    j -= gap
+                arr[j] = temp
+            gap //= 2
+        return arr
         
