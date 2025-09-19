53cc9a0c3ae148afe4f63a07fd9e1b4ceefa91e0
Shravani <tychephp@gmail.com>
2025-09-19 15:06:50 +0530

    Time: 5 ms (25.04%), Space: 19.9 MB (66.33%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,4 +4,8 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        l, r = 0 , len(s)-1
+        while l < r:
+            s[l] , s[r] = s [r], s[l]
+            l+=1
+            r-=1
