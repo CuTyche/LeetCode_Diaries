1a8575d9514781f75aaa3ba532e2ac4a987a2419
Shravani <tychephp@gmail.com>
2025-10-01 23:11:30 +0530

    Time: 305 ms (27.11%), Space: 28.2 MB (64.88%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        output = []
+        l = r = 0
+        q = collections.deque()
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+            if l > q[0]:
+                q.popleft()
+
+            if (r-l+1) >= k:
+                output.append(nums[q[0]])
+                l+=1
+            r+=1
+
+        return output
