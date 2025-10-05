d38eeeaf015babddd6c7b4930eb5462c08905cdf
Shravani <tychephp@gmail.com>
2025-10-05 13:40:13 +0530

    Time: 7 ms (23.41%), Space: 12.5 MB (85.38%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        path = path.split('/')
+
+        for cur in path:
+            if cur == "..":
+                if stack:
+                    stack.pop()
+            elif cur != "" and cur != '.':
+                stack.append(cur)
+        return '/' + '/'.join(stack)
